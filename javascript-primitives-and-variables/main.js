64de318c77d8c20a87f87fbaff380a645e70300c
Dylan De Vei <dylan.devei@gmail.com>
2022-03-21 17:36:31 +0000

    completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,24 @@
+var fullName;
+var isCool;
+var totalPets;
+var unknown;
+var never;
+
+fullName = 'Dylan De Vei';
+isCool = true;
+totalPets = '3';
+unknown = null;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool?:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
