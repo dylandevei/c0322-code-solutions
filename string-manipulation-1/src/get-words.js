199a3d96f506f553e7273a7f5e9d186f2435959c
Dylan De Vei <dylan.devei@gmail.com>
2022-03-25 21:48:39 +0000

    progress.

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -6,4 +6,7 @@ function getWords(string) {
     return [];
   }
 }
-// function to
+// define function with one parameter string
+// function will check if string value is more than zero
+// if more than zero, it will split using .split method to seperate words by spaces ' ';
+// if string is empty it will return empty array
