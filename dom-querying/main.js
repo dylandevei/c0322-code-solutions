9b57a116aeb53ca4f071e3caa1f38d66afeb09b3
Dylan De Vei <dylan.devei@gmail.com>
2022-03-28 17:58:50 +0000

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+var $heading = document.querySelector('h1');
+console.log('$heading:', document.querySelector('h1'));
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log('$explanation:', document.querySelector('#explanation'));
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+console.log('$hint:', document.querySelector('.hint'));
+console.dir($hint);
+
+console.log('$paragraphs:', document.querySelectorAll('p'));
+console.log('$links:', document.querySelectorAll('a'));
