49a7139ac276623c4f7f666c485602d96685e1b6
Dylan De Vei <dylan.devei@gmail.com>
2022-03-29 04:22:35 +0000

    progress string-manipulation-2

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,4 @@
 /* exported numVowels */
+// define function named numVowels with single parameter(string)
+// write function that will count the number of vowels in given string
+// return vowel count
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,16 @@
 /* exported ransomCase */
+
+// define a new function named ransomCase with single parameter (string)
+// create new variable, lowerWord, that will convert string to lowercase and split each character of the word
+// create a for loop that will iterate and capitalized each other letter
+// start loop on 1 so the front letter will remain lowercased and the letter that follows will become uppercase and will skip every other letter from then on
+// uppercase each letter starting after the first letter
+// rejoin the words as ransomCased
+
+function ransomCase(string) {
+  var lowerWord = string.toLowerCase().split('');
+  for (var i = 1; i < lowerWord.length; i += 2) {
+    lowerWord[i] = lowerWord[i].toUpperCase();
+  }
+  return lowerWord.join('');
+}
