4eb559e52861207b154453e23f143dbcbd9c55de
Dylan De Vei <dylan.devei@gmail.com>
2022-03-29 21:18:56 +0000

    progress on javascript-modal exercise.

---
+++ javascript-modal/main.js
@@ -0,0 +1,9 @@
+// var $modalButton = document.querySelector('.open-modal-button');
+// var $popUp = document.querySelector('.popup-window');
+// var $noButton = document.querySelector('.popup-button');
+
+// $modalButton.addEventListener('click', handleClick);
+
+// function handleClick(event) {
+//   $popUp.className = 'popup-window.shown';
+// }
