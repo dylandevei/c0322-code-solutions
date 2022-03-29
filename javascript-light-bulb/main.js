13c3a3e844b43562b2f791a2a719674a298b10ec
Dylan De Vei <dylan.devei@gmail.com>
2022-03-29 23:31:31 +0000

    fixed requests.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,17 +1,20 @@
-var buttonClicks = 0;
-var $background = document.querySelector('body');
+var lightOn = true;
+var $body = document.querySelector('body');
 var $circle = document.querySelector('.circle');
 
 $circle.addEventListener('click', handleClick);
 
 function handleClick(event) {
-  buttonClicks++;
-  var background = null;
-  var circleBackground = null;
-  if (buttonClicks % 2 === 0) {
-    background = 'black';
-    circleBackground = 'black';
+  if (lightOn === true) {
+    $circle.className = 'circle off';
+    $body.className = 'body black';
+    lightOn = false;
+    return;
   }
-  $background.className = 'body ' + background;
-  $circle.className = 'circle ' + circleBackground;
+  if (lightOn === false) {
+    $circle.className = 'circle on';
+    $body.className = 'body white';
+    lightOn = true;
+
+  }
 }
