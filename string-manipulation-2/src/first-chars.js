5c7a3d3275b6f20cce8340f4732ed49dd30e12cd
Dylan De Vei <dylan.devei@gmail.com>
2022-04-01 20:31:17 +0000

    progress strings2

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,8 @@
 /* exported firstChars */
+
+// define function named truncate with two arguments, length and string
+// return substring of the string starting at length given in function arugument
+// first argument will be how many letters you want to keep in the output and use 0 as second argument of the truncate method so it will not take away any letters from the front of the string
+function firstChars(length, string) {
+  return string.substring(length, 0);
+}
