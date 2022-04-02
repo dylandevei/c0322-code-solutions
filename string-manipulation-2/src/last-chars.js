2b0ee81d60d1ae607c232d38b4ffa843df3182a3
Dylan De Vei <dylan.devei@gmail.com>
2022-04-02 20:39:23 +0000

    completed string-manipulation-2 exercise.

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,10 @@
 /* exported lastChars */
+// define a new function named lastChars with two parameters (length) & string
+// use the slice method on string with a negative (length) number and assign the result of that expression to variable newString
+// the string will count from the back up and return the characters once length has counted up to that number
+// return the variable newString
+
+function lastChars(length, string) {
+  var newString = string.slice(-length);
+  return newString;
+}
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,17 @@
 /* exported swapChars */
+
+// define function named swapchars that will swap the index of characters at given index and return string
+// declare new variable that will split string into array
+// declare new variable that will house the firstIndex parameter at a temporary variable
+// assign the firstIndex to equal secondIndex
+// assign second index to equal the firstIndex(temporary variable)
+// use join method to make array turn back into a string
+// return string result
+
+function swapChars(firstIndex, secondIndex, string) {
+  var stringsplit = string.split('');
+  var swapIndex = stringsplit[firstIndex];
+  stringsplit[firstIndex] = stringsplit[secondIndex];
+  stringsplit[secondIndex] = swapIndex;
+  return stringsplit.join('');
+}
