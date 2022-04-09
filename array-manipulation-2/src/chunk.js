cd2dc4dc10f7a09af3c7bb9283798741aace32c0
Dylan De Vei <dylan.devei@gmail.com>
2022-04-09 20:50:10 +0000

    completed array-manipulation-2.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -2,7 +2,7 @@
 
 // define a function named chunk with two parameters, array and size
 // create a new array and make it empty
-// create a for loop that will start at zero, go as long as index is less than the length of the array and increment each time by the size of the size(argument)
+// create a for loop that will start at zero, go as long as index is less than the length of the array and increment each time by the size of the size(argument) and reassign i to new size
 // array with use slice method starting at index and ending at index plus size of the size argument and will push the indexes into new array
 // return newArray
 
