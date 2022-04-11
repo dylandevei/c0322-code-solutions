594177a404cb42e1628587a911cf7c6603f460e5
Dylan De Vei <dylan.devei@gmail.com>
2022-04-11 00:39:32 +0000

    completed object-manipulation-2 exercise.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,14 @@
 /* exported defaults */
+
+// define function with two parameters target and source
+// create for in loop to loop through keys in source
+// if the key in target's key is undefined it will put target's key inside of source object
+// no return
+
+function defaults(target, source) {
+  for (var key in source) {
+    if (target[key] === undefined) {
+      target[key] = source[key];
+    }
+  }
+}
