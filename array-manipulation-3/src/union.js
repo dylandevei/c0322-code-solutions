54c807ff784ffbfcf9360087d6ae9c1da94292e2
Dylan De Vei <dylan.devei@gmail.com>
2022-05-02 04:42:39 +0000

    completed array-manipulation-3.

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,22 @@
 /* exported union */
+
+function union(first, second) {
+  var solution = [];
+
+  for (var i = 0; i < first.length; i++) {
+    if (!solution.includes(first[i])) {
+      solution.push(first[i]);
+    }
+  }
+  for (var k = 0; k < second.length; k++) {
+    if (!solution.includes(second[k])) {
+      solution.push(second[k]);
+    }
+  }
+  return solution;
+}
+
+// create new solution array
+// iterate through first array and check if solution array includes indexes, if not push to solution array
+// iterate through second array and check if solution array includes indexes, if not push to solution array
+// return solution array
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,17 @@
 /* exported unique */
+
+function unique(array) {
+  var solution = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (!solution.includes(array[i])) {
+      solution.push(array[i]);
+    }
+  }
+  return solution;
+}
+
+// create empty array
+// loop through parameter array and check if index is in solution array
+// if not included, push index into solution array (no repeated values pushed)
+// return solution array
