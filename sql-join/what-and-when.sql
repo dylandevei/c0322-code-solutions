f157db7276329899bf391803f1c4c12521f7356d
Dylan De Vei <dylan.devei@gmail.com>
2022-05-12 03:34:01 +0000

    completed sql-join exercise

--- sql-join/what-and-when.sql
+++ sql-join/what-and-when.sql
@@ -1,4 +1,6 @@
-select "releaseYear"
+select "releaseYear",
+      "genres"."name"
         from "films"
        join "filmGenre" using ("filmId")
+       join "genres" using ("genreId")
        where "title" = 'Boogie Amelie';
