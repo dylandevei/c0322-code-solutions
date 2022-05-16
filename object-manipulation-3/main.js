c498e1cbf871f0b7e6204c37b8d0d283a416569f
Dylan De Vei <dylan.devei@gmail.com>
2022-05-16 04:23:27 +0000

    progress objects-3

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,27 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+const players = [
+  {
+    layer: 'Player 1',
+    hand: [],
+    score: 0
+  },
+  {
+    player: 'Player 2',
+    hand: [],
+    score: 0
+  },
+  {
+    player: 'Player 3',
+    hand: [],
+    score: 0
+  },
+  {
+    player: 'Player 4',
+    hand: [],
+    score: 0
+  }
+];
+
+const deck = [];
+const suit = ['spades', 'clubs', 'diamonds', 'hearts'];
+const rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
