59944f9c0b94c33a18ecefc5f99824d83728ba04
Dylan De Vei <dylan.devei@gmail.com>
2022-05-17 00:21:28 +0000

    completed react-elements exercise

---
+++ react-elements/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const container = document.querySelector('#root');
+
+const element = React.createElement(
+  'h1',
+  [null],
+  ['Hello, React!']
+);
+
+console.log(element);
+
+const root = ReactDOM.createRoot(container);
+root.render(element);
