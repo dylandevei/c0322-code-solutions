d3dcc01c63e0690140e7d8b87858ee87a7176117
Dylan De Vei <dylan.devei@gmail.com>
2022-05-20 20:12:10 +0000

    completed react-app-drawer-component challenge

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { clicks: 0 };
+    this.state = { sidebarOpen: false };
 
     this.handleClick =
       this.handleClick.bind(this);
@@ -11,32 +11,68 @@ class AppDrawer extends React.Component {
 
   handleClick() {
     this.setState(prevState => ({
-      clicks: this.state.clicks + 1
+      sidebarOpen: !this.state.sidebarOpen
     }));
   }
 
   render() {
-    return (
-      <div>
-      <header>
-        <div className='navbar'>
-          <div>
-              <button><i className="fa-solid fa-bars"></i></button>
+    if (!this.state.sidebarOpen) {
+      return (
+        <div>
+          <div className='sidebar-off'>
+            <div className='sidebar-text'>
+              <h1>Settings</h1>
+              <ul onClick={this.handleClick}>
+                <h4>About Us</h4>
+                <h4>Contact Us</h4>
+                <h4>Our Story</h4>
+              </ul>
+            </div>
           </div>
-        </div>
-      </header>
-        <div className='container'>
-      <div className='row'>
-        <div className='column-full'>
-          <div className='img'>
-              <img src="https://pbs.twimg.com/media/FRXaiWUWUAIMXEO.jpg:large" alt="" />
+          <header>
+            <button onClick={this.handleClick}><i className="fa-solid fa-bars"></i></button>
+          </header>
+          <div className='container'>
+            <div className='row'>
+              <div className='column-full'>
+                <div className='img'>
+                  <img src="https://pbs.twimg.com/media/FRXaiWUWUAIMXEO.jpg:large" alt="kirby and friends" />
+                </div>
+              </div>
+            </div>
           </div>
+          <div className='overlay' onClick={this.handleClick}></div>
         </div>
-      </div>
-    </div>
-      </div>
-
-    );
+      );
+    } else {
+      return (
+        <div>
+          <div className='sidebar-on'>
+            <div className='sidebar-text'>
+              <h1>Settings</h1>
+              <ul onClick={this.handleClick}>
+                <h4>About Us</h4>
+                <h4>Contact Us</h4>
+                <h4>Our Story</h4>
+              </ul>
+            </div>
+          </div>
+          <header>
+                <button><i className="fa-solid fa-bars"></i></button>
+          </header>
+          <div className='container'>
+            <div className='row'>
+              <div className='column-full'>
+                <div className='img'>
+                  <img src="https://pbs.twimg.com/media/FRXaiWUWUAIMXEO.jpg:large" alt="kirby and friends" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='overlay-on' onClick={this.handleClick}></div>
+        </div>
+      );
+    }
   }
 }
 
