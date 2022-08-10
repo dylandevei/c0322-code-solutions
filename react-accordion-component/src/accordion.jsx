3f93f3dda24bbee4b38ca7e6af8a6cc4cefcd129
Dylan De Vei <dylan.devei@gmail.com>
2022-08-10 00:53:13 +0000

    completed react-accordion-component exercise

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,79 +1,63 @@
 import React from 'react';
 
-class AppDrawer extends React.Component {
+class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { sidebarOpen: false };
-
-    this.handleClick =
-      this.handleClick.bind(this);
+    this.state = {
+      index: null
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      sidebarOpen: !this.state.sidebarOpen
-    }));
+  handleClick(event) {
+    const movieId = event.target.getAttribute('data-view');
+    if (this.state.index === movieId) {
+      this.setState({
+        index: null
+      });
+    } else {
+      this.setState({
+        index: movieId
+      });
+    }
   }
 
   render() {
-    if (!this.state.sidebarOpen) {
-      return (
-        <div>
-          <div className='sidebar-off'>
-            <div className='sidebar-text'>
-              <h1>Settings</h1>
-              <ul onClick={this.handleClick}>
-                <h4>About Us</h4>
-                <h4>Contact Us</h4>
-                <h4>Our Story</h4>
-              </ul>
-            </div>
-          </div>
-          <header>
-            <button onClick={this.handleClick}><i className="fa-solid fa-bars"></i></button>
-          </header>
-          <div className='container'>
-            <div className='row'>
-              <div className='column-full'>
-                <div className='img'>
-                  <img src="https://pbs.twimg.com/media/FRXaiWUWUAIMXEO.jpg:large" alt="kirby and friends" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='overlay' onClick={this.handleClick}></div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className='sidebar-on'>
-            <div className='sidebar-text'>
-              <h1>Settings</h1>
-              <ul onClick={this.handleClick}>
-                <h4>About Us</h4>
-                <h4>Contact Us</h4>
-                <h4>Our Story</h4>
-              </ul>
-            </div>
-          </div>
-          <header>
-            <button><i className="fa-solid fa-bars"></i></button>
-          </header>
-          <div className='container'>
-            <div className='row'>
-              <div className='column-full'>
-                <div className='img'>
-                  <img src="https://pbs.twimg.com/media/FRXaiWUWUAIMXEO.jpg:large" alt="kirby and friends" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='overlay-on' onClick={this.handleClick}></div>
-        </div>
-      );
-    }
+    return (
+      <div className='accordion-div'>
+        <ul>
+          {
+            this.props.movies.map((movie, index) => {
+              if (this.state.index === movie.title) {
+                return (
+                  <li key={index} id={index}>
+                    <div className='accordion-header'>
+                      <h1 data-view={movie.title} onClick={this.handleClick}> {movie.title} </h1>
+                    </div>
+                    <div className="accordion-bio">
+                      <img src={movie.poster} alt={movie.title} />
+                      <p className='accordion-text'>{movie.plot}</p>
+                    </div>
+                  </li>
+                );
+              }
+              return (
+                <li key={index} id={index}>
+                  <div className='accordion-header'>
+                    <h1 data-view={movie.title} onClick={this.handleClick}>{movie.title}</h1>
+                  </div>
+                  <div className="accordion-bio hidden">
+                    <img src={movie.poster} alt={movie.title} />
+                    <p className='accordion-text'>{movie.plot}</p>
+                  </div>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
   }
 }
 
-export default AppDrawer;
+export default Accordion;
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Accordion from './accordion';
 
+const movies = [
+  {
+    title: 'Spiderman: Homecoming',
+    plot: 'Thrilled by his experience with the Avengers, young Peter Parker returns home to live with his Aunt May. Under the watchful eye of mentor Tony Stark, Parker starts to embrace his newfound identity as Spider-Man. He also tries to return to his normal daily routine -- distracted by thoughts of proving himself to be more than just a friendly neighborhood superhero. Peter must soon put his powers to the test when the evil Vulture emerges to threaten everything that he holds dear.',
+    poster: 'https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg'
+
+  },
+  {
+    title: 'Spiderman: Far From Home',
+    plot: 'Peter Parker\'s relaxing European vacation takes an unexpected turn when Nick Fury shows up in his hotel room to recruit him for a mission.The world is in danger as four massive elemental creatures -- each representing Earth, air, water and fire-- emerge from a hole torn in the universe.Parker soon finds himself donning the Spider - Man suit to help Fury and fellow superhero Mysterio stop the evil entities from wreaking havoc across the continent.',
+    poster: 'https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg'
+  },
+  {
+    title: 'Spiderman: No Way Home',
+    plot: 'With Spider-Man\'s identity now revealed, our friendly neighborhood web- slinger is unmasked and no longer able to separate his normal life as Peter Parker from the high stakes of being a superhero.When Peter asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider - Man.',
+    poster: 'https://upload.wikimedia.org/wikipedia/en/0/00/Spider-Man_No_Way_Home_poster.jpg'
+
+  }
+];
+
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<Accordion />);
+root.render(<Accordion movies={movies} />);
